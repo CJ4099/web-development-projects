f507450d33a2ab8e069c847c09f0399bb6d19d48
CJ4099 <nyawangacollins53@gmail.com>
2026-05-05 15:19:19 +0300

    Add files via upload

---
+++ ec.js
@@ -0,0 +1,13 @@
+let total = 0;
+
+function addToCart(name, price) {
+  let cart = document.getElementById("cart");
+
+  let li = document.createElement("li");
+  li.innerText = name + " - " + price;
+  cart.appendChild(li);
+
+  total += price;
+  document.getElementById("total").innerText = total;
+}
+
